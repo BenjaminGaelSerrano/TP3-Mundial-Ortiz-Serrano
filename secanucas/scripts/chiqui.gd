fda96e8117f02f0cb0578c27f0fada2e1bffd549
Benjamín-Serrano <alumno26.serrano.benjamin@ipm.edu.ar>
2026-05-19 17:34:38 -0300

    Adicion de nuevas funciones en el movimiento del Chiqui

--- secanucas/scripts/chiqui.gd
+++ secanucas/scripts/chiqui.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 enum estado {CAMINAR,QUIETO}
 var estadoTapia= estado.CAMINAR
-const velocidad=80
+const velocidad=180
 var direccion= Vector2.RIGHT
-var timer=8.0
+var timer
+var colorVigilanciaNormal=Color(0, 0.5, 1, 0.4)
+var colorVigilanciaAlerta=Color(1, 0, 0, 0.4)
 @onready var animacion= $Animacion
 @onready var detectorIzquierda= $DetectorDeObstaculosIzquierda
 @onready var detectorCentro= $DetectorDeObstaculosFrente
 @onready var detectorDerecha= $DetectorDeObstaculosDerecha
+@onready var poligono= $VigilanciaArea/FormaVigilancia
+func _ready() -> void:
+	timer=randf_range(3.5, 12.0)
+	poligono.color= colorVigilanciaNormal
 func _physics_process(delta: float) -> void:
+	poligono.rotation = direccion.angle()-PI/2
 	if(estadoTapia==estado.CAMINAR):
 		esquivarParedes()
 		velocity= direccion*velocidad
@@ -17,18 +24,18 @@ func _physics_process(delta: float) -> void:
 		timer-=delta
 		if timer<=0.0:
 			estadoTapia=estado.QUIETO
-			timer=3.0
-			moverseAlAzar()
+			timer=2.0
+			direccion=-direccion
 	elif estadoTapia==estado.QUIETO :
 		velocity=Vector2.ZERO
 		animacionQuieto()
 		timer-=delta
 		if timer<=0:
 			estadoTapia=estado.CAMINAR
-			timer=8.0
+			timer=randf_range(3.5, 12.0)
+			moverseAlAzar()
 func moverseAlAzar():
-	var movimientos= [Vector2.RIGHT,Vector2.DOWN,Vector2.LEFT,Vector2.UP]
-	direccion = movimientos.pick_random()			
+	pass		
 func animacionCaminar():
 	if direccion == Vector2.UP :
 		animacion.play("CaminarAtras")
@@ -48,15 +55,23 @@ func animacionQuieto():
 	elif direccion == Vector2.LEFT :
 		animacion.play("IzquierdaParado")						
 func esquivarParedes():
-	detectorCentro.target_position = direccion*40
-	detectorIzquierda.target_position= direccion.rotated(deg_to_rad(-90))*40
-	detectorDerecha.target_position= direccion.rotated(deg_to_rad(90))*40
+	var largoRayCast=25
+	detectorCentro.target_position = direccion*largoRayCast
+	detectorIzquierda.target_position= Vector2(direccion.y, -direccion.x).sign()*largoRayCast
+	detectorDerecha.target_position= Vector2(-direccion.y, direccion.x).sign()*largoRayCast
 	detectorCentro.force_raycast_update()
 	detectorIzquierda.force_raycast_update()
 	detectorDerecha.force_raycast_update()
 	if detectorCentro.is_colliding():
-		direccion = direccion.rotated(deg_to_rad(90)).round()
-	elif detectorIzquierda.is_colliding():
-		direccion = direccion.rotated(deg_to_rad(45)).round()								
-	elif detectorDerecha.is_colliding():
-		direccion = direccion.rotated(deg_to_rad(-45)).round()
+		if not detectorDerecha.is_colliding():
+			direccion = Vector2(-direccion.y, direccion.x).sign()								
+		elif not detectorIzquierda.is_colliding():
+			direccion = Vector2(direccion.y, -direccion.x).sign()
+		else :
+			direccion=-direccion.sign()	
+func _on_vigilancia_area_body_entered(body: Node2D) -> void:
+	if body.name == "Jugador": 
+		poligono.color = colorVigilanciaAlerta
+func _on_vigilancia_area_body_exited(body: Node2D) -> void:
+	if body.name == "Jugador":
+		poligono.color = colorVigilanciaNormal
